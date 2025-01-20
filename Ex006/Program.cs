9220144b4bec7c6559199d5ab72fa1901a59e2d4
Ana Beatriz <contatobeatrizcouto@gmail.com>
2025-01-20 19:41:30 -0300

    Exercicio do teste do triamgulo

---
+++ Ex006/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Ex006
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            float media; 
+            Console.WriteLine(" digite uma nota: ");    
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(" digite outra nota: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            media = (num1 + num2) / 2;
+            Console.WriteLine("A média é: " + media); 
+
+
+        }
+    }
+}
---
+++ Testetriangulo/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Testetriangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ladoA, ladoB, ladoC;
+            Console.Write("Digite o valor do ladoA: ");
+            ladoA = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o valor do ladoB: ");
+            ladoB = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o valor do ladoC: ");
+            ladoC = Convert.ToInt32(Console.ReadLine());
+
+
+        }
+    }
+}
