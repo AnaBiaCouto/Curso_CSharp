66d747e8809487c94e9e882cc9a66a0d11853640
Ana Beatriz <contatobeatrizcouto@gmail.com>
2025-01-20 22:22:03 -0300

    Aumento de salario com valores variados

---
+++ AumentoSalario/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace AumentoSalario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual o valor do seu salario?");
+            double salario;
+            salario = Convert.ToDouble(Console.ReadLine());
+            double aumento;
+
+            if (salario > 2500)
+            {
+                aumento = salario * 0.05;
+            }
+            else if (salario > 1250)
+            {
+                aumento = salario * 0.10;
+            }
+            else
+            {
+                aumento = salario * 0.15;
+            }
+
+            Console.WriteLine("O aumento do seu salario é de: {0}", aumento + salario);
+
+
+
+
+
+
+
+        }
+    }
+}
