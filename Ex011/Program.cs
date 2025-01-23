cfc2963dd0d54d6c8e3512c7d01e26b2d76beb34
Ana Beatriz <contatobeatrizcouto@gmail.com>
2025-01-22 21:02:30 -0300

    Novos exercícios

---
+++ Ex011/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Ex011
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("===================");
+            Console.WriteLine("Contagem Regressiva");
+            Console.WriteLine("===================");
+
+            for (int i = 10; i >0; i --)
+            {
+                Console.WriteLine(i);
+            }
+                
+                
+            
+        }
+    }
+}
