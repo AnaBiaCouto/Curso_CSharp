e9a4fb1cdc2a3d4513ef9cba337abe15fd6af003
Ana Beatriz <contatobeatrizcouto@gmail.com>
2025-01-22 21:34:47 -0300

    Exercicio de tabuada

---
+++ Ex014/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Tabuadafor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numero;
+            Console.WriteLine("Digite um número");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("{0} x {1} = {2}", numero, i, numero * i);
+            }
+
+        }
+    }
+}
