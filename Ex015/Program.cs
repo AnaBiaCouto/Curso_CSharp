0799ec4cd1671a75fb712102c12c58d81d43181d
Ana Beatriz <contatobeatrizcouto@gmail.com>
2025-01-22 21:58:29 -0300

    Exercicios de genero

---
+++ Ex015/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Ex015
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string genero; 
+
+            Console.Write("Digite o seu gênero: ");
+            genero = Console.ReadLine().ToUpper();
+
+            while ( genero != "F" && genero != "M")
+            {
+                Console.Write("Digite um gênero: ");
+                genero = Console.ReadLine();
+            }
+
+            Console.WriteLine("Bem vindos e bem vindas ao curso de C#");
+        } 
+    }
+}
